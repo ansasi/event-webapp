6f4bfb670a41ff03ad5919f9ff07c6c3ba1b0922
ansasi <ansasi1998@gmail.com>
2024-02-05 00:13:56 +0100

    eventform

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-// import CategoryFilter from '@/components/shared/CategoryFilter';
-// import Collection from '@/components/shared/Collection'
-// import Search from '@/components/shared/Search';
+import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
-// import { getAllEvents } from '@/lib/actions/event.actions';
-// import { SearchParamProps } from '@/types';
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -38,9 +38,9 @@ export default function Home() {
         {/* <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
           <CategoryFilter />
-        </div>
+        </div> */}
 
-        <Collection
+        {/* <Collection
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
--- lib/validator.ts
+++ lib/validator.ts
@@ -10,5 +10,5 @@ export const eventFormSchema = z.object({
   categoryId: z.string(),
   price: z.string(),
   isFree: z.boolean(),
-  url: z.string().url()
+  url: z.string().nullable(),
 })
